4a9093c9dc7f5543091b911acef11bf9e8d55344
MIX-1 <danilonil1@yandex.ru>
2023-06-04 22:16:12 +0300

    Add new functionality. Write to console condition of sectors and number spins

--- circle.js
+++ circle.js
@@ -12,21 +12,31 @@ const sectorAngle = Math.PI / 2;
 const centerCircle = document.querySelector('.center-circle');
 
 // массив данных о секторе
+let simpleCircle;
 let arraySectorsData = [];
 
+// конструктор для данных круга
+function SimpleCircle(spinCounter, arraySectorsData) {
+    this.spinCounter = spinCounter;
+    this.arraySectorsData = arraySectorsData;
+}
+
 // конструктор для данных сектора
-function SectorData(imgLink, color, num1, num2) {
+function SectorData(id, X, Y, imgLink, color, num1, num2) {
+    this.id = id;
+    this.X = X;
+    this.Y = Y;
     this.imgLink = imgLink;
     this.text = `${num1} : ${num2}`;
     this.color = color;
 }
 
-// генерация случайного целого числа от min до max
+// Генерация случайного целого числа от min до max
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//читаем JSON и заполняем объекты
+// Читаем JSON и заполняем объекты
 function readJSON() {
 // Создаем новый объект XMLHttpRequest
     const xhr = new XMLHttpRequest();
@@ -43,16 +53,74 @@ function readJSON() {
         if (xhr.status === 200) {
             // Парсим содержимое файла JSON в объект
             const json = JSON.parse(xhr.responseText);
-            for (const obj of json) {
-                let sectorData = new SectorData(obj.link, obj.color,getRandomInt(1, 10), getRandomInt(1, 10));
+            for (const obj of json["sectors"]) {
+                let sectorData = new SectorData(obj.id, obj.X, obj.Y, obj.link, obj.color,getRandomInt(1, 10), getRandomInt(1, 10));
                 arraySectorsData.push(sectorData);
             }
-            console.log(arraySectorsData)
+            console.log(arraySectorsData);
+            simpleCircle = new SimpleCircle(json["spins_count"], arraySectorsData);
+            console.log(simpleCircle);
+            console.log(`Total spin count: ${simpleCircle.spinCounter}`);
         }
     };
 
 // Отправляем запрос на сервер
     xhr.send();
+}
+
+// Пишем JSON
+function writeJSON() {
+    const data = {
+        spins_count: simpleCircle.spinCounter,
+        sectors: [
+            {
+                "id":    simpleCircle.arraySectorsData[0].id,
+                "X":     simpleCircle.arraySectorsData[0].X,
+                "Y":     simpleCircle.arraySectorsData[0].Y,
+                "color": simpleCircle.arraySectorsData[0].color,
+                "link":  simpleCircle.arraySectorsData[0].imgLink
+            },
+            {
+                "id":    simpleCircle.arraySectorsData[1].id,
+                "X":     simpleCircle.arraySectorsData[1].X,
+                "Y":     simpleCircle.arraySectorsData[1].Y,
+                "color": simpleCircle.arraySectorsData[1].color,
+                "link":  simpleCircle.arraySectorsData[1].imgLink
+            },
+            {
+                "id":   simpleCircle.arraySectorsData[2].id,
+                "X":    simpleCircle.arraySectorsData[2].X,
+                "Y":    simpleCircle.arraySectorsData[2].Y,
+                "color":simpleCircle.arraySectorsData[2].color,
+                "link": simpleCircle.arraySectorsData[2].imgLink
+            },
+            {
+                "id":   simpleCircle.arraySectorsData[3].id,
+                "X":    simpleCircle.arraySectorsData[3].X,
+                "Y":    simpleCircle.arraySectorsData[3].Y,
+                "color":simpleCircle.arraySectorsData[3].color,
+                "link": simpleCircle.arraySectorsData[3].imgLink
+            }
+        ]
+    };
+    const jsonString = JSON.stringify(data, null, 4);
+    console.log("JSON str:\n" + jsonString);
+}
+
+// Сопоставление пары x-y и пары startAngle-endAngle
+function nextCoordinatesPair(x, y) {
+    if(x === 1 && y === 1) return { X: 0, Y: 1 };
+    if(x === 0 && y === 1) return { X: 0, Y: 0 };
+    if(x === 0 && y === 0) return { X: 1, Y: 0 };
+    if(x === 1 && y === 0) return { X: 1, Y: 1 };
+}
+
+// Сопоставление пары x-y и пары startAngle-endAngle
+function translateCoordinates(x, y) {
+    if(x === 1 && y === 1) return { startAngle: 0, endAngle: sectorAngle };
+    if(x === 0 && y === 1) return { startAngle: sectorAngle, endAngle: 2 * sectorAngle };
+    if(x === 0 && y === 0) return { startAngle: 2 * sectorAngle, endAngle: 3 * sectorAngle };
+    if(x === 1 && y === 0) return { startAngle: 3 * sectorAngle, endAngle: 4 * sectorAngle };
 }
 
 // Закрасить круг
@@ -63,8 +131,11 @@ function fillCircleWhite() {
     ctx.fill();
 }
 
-// Отрисовка секторов и текста внутри них
-function drawSector(startAngle, endAngle, color, text, imgLink) {
+// Отрисовка сектора, его картинки и текста внутри
+function drawSector(X, Y, color, text, imgLink) {
+    let sectorAngles = translateCoordinates(X, Y);
+    let startAngle = sectorAngles["startAngle"];
+    let endAngle = sectorAngles["endAngle"];
     ctx.save();
     ctx.beginPath();
     ctx.moveTo(centerX, centerY);
@@ -75,7 +146,7 @@ function drawSector(startAngle, endAngle, color, text, imgLink) {
     ctx.restore();
 
     ctx.fillStyle = '#000';
-    ctx.font = '10px Arial';
+    ctx.font = '12px Arial';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
     const textX = centerX + (radius / 2) * Math.cos((startAngle + endAngle) /  2);
@@ -90,18 +161,28 @@ function drawSector(startAngle, endAngle, color, text, imgLink) {
     }
 }
 
+// Меняет координаты для поворота по часовой стрелке
+function turnClockwise() {
+    for (const sector of simpleCircle.arraySectorsData){
+        let nextCoordinates = nextCoordinatesPair(sector.X, sector.Y);
+        sector.X = nextCoordinates["X"];
+        sector.Y = nextCoordinates["Y"];
+    }
+}
+
+// Отрисовывает все сектора
 function drawSectors() {
-    drawSector(0, sectorAngle, arraySectorsData[0].color, arraySectorsData[0].text, arraySectorsData[0].imgLink);
-    drawSector(sectorAngle, 2 * sectorAngle, arraySectorsData[1].color, arraySectorsData[1].text, arraySectorsData[1].imgLink);
-    drawSector(2 * sectorAngle, 3 * sectorAngle, arraySectorsData[2].color, arraySectorsData[2].text, arraySectorsData[2].imgLink);
-    drawSector(3 * sectorAngle, 4 * sectorAngle, arraySectorsData[3].color, arraySectorsData[3].text, arraySectorsData[3].imgLink);
+    for (const sector of simpleCircle.arraySectorsData)
+        drawSector(sector.X, sector.Y, sector.color, sector.text, sector.imgLink);
 }
 
 // Функция для изменения порядка секторов при клике на центральный круг
 function changeSectorsOrder() {
     fillCircleWhite();
-    arraySectorsData.unshift(arraySectorsData.pop());
-    drawSectors()
+    turnClockwise()
+    drawSectors();
+    ++simpleCircle.spinCounter;
+    writeJSON();
 }
 
 centerCircle.addEventListener('click', changeSectorsOrder);
